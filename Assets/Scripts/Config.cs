bbd796c8459e4f1b14af8de6a6c8021a31e1e443
Zhirui Li <zr.public@outlook.com>
2018-08-18 16:01:08 +0800

    Revert "Revert "添加配置文件""
    This reverts commit 1169e5cd7e0fe6adb52279cff823dc3a7041efed.

---
+++ Assets/Scripts/Config.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+[System.Serializable]
+public class Config {
+	
+	private static string fileName = "config.json";
+
+	public float MoveSpeed;
+	public float DragTimeThreshold;
+	public float OffsetThreshold;
+
+	public static Config LoadFromFile()
+	{
+		string fullPath = Path.Combine (Application.streamingAssetsPath, fileName);
+		if (File.Exists (fullPath)) {
+			string dataAsJson = File.ReadAllText (fullPath);
+			return JsonUtility.FromJson<Config>(dataAsJson);
+		} else {
+			Debug.LogError ("Can't load config file from path: " + fullPath);
+			return null;
+		}
+	}
+
+	public void SaveToFile(Config config)
+	{
+		string fullPath = Path.Combine (Application.streamingAssetsPath, fileName);
+		string text = "";
+		FileInfo file = new FileInfo(fullPath);
+		if (file.Exists) {
+			text = JsonUtility.ToJson (config);
+			StreamWriter w = new StreamWriter (fullPath);
+			w.Write (text);
+			w.Close ();
+		} else {
+			Debug.LogError ("No such file: " + fullPath);
+		}
+	}
+}
