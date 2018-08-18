cbb16684ffe3be6be508a36f1b0afafbfc019171
Zhirui Li <zr.public@outlook.com>
2018-08-18 17:13:57 +0800

    将进度信息实现为单例

--- Assets/Scripts/PlayerProgress.cs
+++ Assets/Scripts/PlayerProgress.cs
@@ -1,9 +1,27 @@
-﻿// 玩家进度信息
+﻿// 玩家进度信息单例
+// 
+// 调用方式：
+//    var mainLv = PlayerProgress.Instance.mainLevel;
+//    PlayerProgress.Instance.mainLevel = mainLv;
+//    PlayerProgress.Instance.Save();
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerProgress {
+
+	private PlayerProgress() {
+		Load ();
+	}
+
+	public static PlayerProgress Instance { get { return Nested.instance; } }
+
+	private class Nested
+	{
+		static Nested() {}
+
+		internal static readonly PlayerProgress instance = new PlayerProgress();
+	}
 
 	// 是否玩过（会影响首页是否显示“继续游戏”按钮）
 	public bool hasPlayed;
@@ -12,30 +30,34 @@ public class PlayerProgress {
 	// 子关卡等级
 	public int subLevel;
 
-	private const string MAIN_LEVEL_KEY = "MainLevel";
-	private const string SUB_LEVEL_KEY = "SubLevel";
-
-	// 读取玩家信息，不会返回空
-	public static PlayerProgress Load()
+	// 重新读取玩家信息
+	public void Reload()
 	{
-		var progress = new PlayerProgress ();
-		if (PlayerPrefs.HasKey (MAIN_LEVEL_KEY) &&
-		    PlayerPrefs.HasKey (SUB_LEVEL_KEY)) {
-			progress.mainLevel = PlayerPrefs.GetInt (MAIN_LEVEL_KEY);
-			progress.subLevel = PlayerPrefs.GetInt (SUB_LEVEL_KEY);
-			progress.hasPlayed = true;
-		} else {
-			progress.mainLevel = 0;
-			progress.subLevel = 0;
-			progress.hasPlayed = false;
-		}
-		return progress;
+		Load ();
 	}
 
 	// 保存玩家信息
-	public void SaveProgress()
+	public void Save()
 	{
 		PlayerPrefs.SetInt (MAIN_LEVEL_KEY, mainLevel);
 		PlayerPrefs.SetInt (SUB_LEVEL_KEY, subLevel);
 	}
+
+	// 读取玩家信息
+	private void Load()
+	{
+		if (PlayerPrefs.HasKey (MAIN_LEVEL_KEY) &&
+		    PlayerPrefs.HasKey (SUB_LEVEL_KEY)) {
+			mainLevel = PlayerPrefs.GetInt (MAIN_LEVEL_KEY);
+			subLevel = PlayerPrefs.GetInt (SUB_LEVEL_KEY);
+			hasPlayed = true;
+		} else {
+			mainLevel = 0;
+			subLevel = 0;
+			hasPlayed = false;
+		}
+	}
+
+	private const string MAIN_LEVEL_KEY = "MainLevel";
+	private const string SUB_LEVEL_KEY = "SubLevel";
 }
