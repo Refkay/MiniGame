d9ef3baf46c02359462d17d73fc94cd6d046cc02
Refkay <1904362965@qq.com>
2018-08-18 13:57:58 +0800

    移动脚本

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,103 @@
+﻿using UnityEngine;
+using System.Collections;
+using HedgehogTeam.EasyTouch;
+using UnityEngine.UI;
+
+public class Move : MonoBehaviour
+{
+
+    public Rigidbody2D mRg2D;
+
+    //能否能移动
+    private bool isMoveable;
+
+    private Vector2 mStartPosition;
+
+    private Vector2 mTargetPosition;
+
+    private Vector2 mMoveDirection;
+
+    //物体移动的时间
+    [SerializeField]
+    private float mMoveSpeed = 500.0f;
+
+    //滑动时间
+    private float mDragTime = 0.0f;
+
+    //滑动时间的阈值
+    [SerializeField]
+    private float mDragTimeThreshold = 0.5f;
+
+    //滑动距离
+    private float mOffsetDistance = 0.0f;
+
+    //滑动距离的阈值
+    [SerializeField]
+    private float mOffsetThreshold = 100.0f;
+
+    //最小可转向距离，滑动必须超过这个距离才可以进行滑动
+    [SerializeField]
+    private float mMinoffset = 30.0f;
+
+    void Start()
+    {
+        mRg2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * 3);
+
+    }
+
+    private void OnEnable()
+    {
+        EasyTouch.On_TouchStart += On_TouchStart;
+        EasyTouch.On_TouchDown += On_TouchDown;
+        EasyTouch.On_TouchUp += On_TouchUp;
+    }
+
+    private void On_TouchStart(Gesture gesture)
+    {
+        mStartPosition = gesture.position;
+    }
+
+    private void On_TouchDown(Gesture gesture)
+    {
+        mTargetPosition = gesture.position;
+        mMoveDirection = mTargetPosition - mStartPosition;
+        mDragTime += Time.deltaTime;
+        mOffsetDistance = mMoveDirection.magnitude;
+        //滑动的时候超过时间的阈值或者超过距离的阈值，球也要转向      
+        if (((mDragTime > mDragTimeThreshold) || (mOffsetDistance > mOffsetThreshold)) && isMoveable)
+        {
+            mMoveDirection.Normalize();
+            mRg2D.velocity = mMoveDirection * mMoveSpeed;
+            mDragTime = 0;
+            mOffsetDistance = 0;
+            isMoveable = false;
+        }
+    }
+
+    private void On_TouchUp(Gesture gesture)
+    {
+        //滑动时间归0
+        mDragTime = 0;
+        //偏移量归0
+        mOffsetDistance = 0;
+        mTargetPosition = gesture.position;
+        mMoveDirection = mTargetPosition - mStartPosition;
+        //必须滑动到一定的距离才能转向
+        if (mMoveDirection.magnitude >= mMinoffset)
+        {
+            mRg2D.Sleep();
+            mMoveDirection.Normalize();
+            //mRg2d.AddForce(moveDirection * mMoveSpeed);
+            mRg2D.velocity = mMoveDirection * mMoveSpeed;
+            isMoveable = true;
+        }
+
+    }
+
+   
+}
---
+++ Assets/Scripts/MoveControl.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveControl : MonoBehaviour
+{
+
+
+    [SerializeField]
+    private ETCJoystick joystick;//虚拟摇杆
+    //[SerializeField]
+ 
+
+    [SerializeField]
+    private float moveSpeed = 100.0f;//移动速度
+    [SerializeField]
+    private float rotSpeed = 60;//移动速度
+    [SerializeField]
+    private Rigidbody2D rb2d;
+    [SerializeField]
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        //joystick.onMoveEnd.AddListener(() => onMoveEnd());
+
+        ////方式一：按键方法注册
+        //joystick.OnPressLeft.AddListener(() => JoystickHandlerMoving());
+        //joystick.OnPressRight.AddListener(() => JoystickHandlerMoving());
+        //joystick.OnPressUp.AddListener(() => JoystickHandlerMoving());
+        //joystick.OnPressDown.AddListener(() => JoystickHandlerMoving());
+
+    }
+
+    //方式二：输入监测
+    void FixedUpdate()
+    {
+        if (ETCInput.GetAxisPressedUp("Vertical"))
+        {
+            JoystickHandlerMoving();
+        }
+
+
+        if (ETCInput.GetAxisPressedDown("Vertical"))
+        {
+            JoystickHandlerMoving();
+        }
+
+
+        if (ETCInput.GetAxisPressedLeft("Horizontal"))
+        {
+            JoystickHandlerMoving();
+
+        }
+        if (ETCInput.GetAxisPressedRight("Horizontal"))
+        {
+            JoystickHandlerMoving();
+
+        }
+
+
+    }
+
+
+
+
+    void JoystickHandlerMoving()
+    {
+        //if (joystick.name != "New Joystick")
+        //{
+        //    return;
+        //}
+
+        //获取虚拟摇杆偏移量  
+        float h = joystick.axisX.axisValue;
+        float v = joystick.axisY.axisValue;
+
+        Vector3 moveDirection = new Vector3(h, v, 0);
+        moveDirection.Normalize();
+        float step = rotSpeed * Time.fixedDeltaTime;
+        if (Mathf.Abs(h) > 0.05f || (Mathf.Abs(v) > 0.05f))
+        {              
+            rb2d.AddForce(moveDirection * moveSpeed);           
+        }
+    }
+
+
+}
