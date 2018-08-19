8c1e39683b017f9f51a969badcad2f2bf9ed7172
zhiruili <zr.public@outlook.com>
2018-08-19 15:44:25 +0800

    添加主界面

---
+++ Assets/Scripts/UI/LevelButtonCtrl.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelButtonCtrl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        var btn = GetComponent<UIButton>();
+        btn.state = UIButton.State.Disabled;
+        // 如果不取消掉碰撞器，那么鼠标滑过按钮的时候还会变成 hover 状态
+        GetComponent<BoxCollider>().enabled = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
