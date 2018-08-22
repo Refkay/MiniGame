bc0337c57705db0c60ff8cdbdb6a3ac732abde5f
Refkay <1904362965@qq.com>
2018-08-22 16:16:06 +0800

    因为版本问题还原场景

---
+++ Assets/Scripts/BlackHole.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlackHole : MonoBehaviour {
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Vector3 gravity = (transform.position - collision.gameObject.transform.position).normalized;
+            collision.gameObject.GetComponent<Rigidbody2D>().AddForce(gravity * 20);
+        }
+    }
+}
