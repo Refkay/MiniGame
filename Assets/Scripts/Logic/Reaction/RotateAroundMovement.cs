264d87d1d23c97fe484387f2678a9d4e51ef1853
Refkay <1904362965@qq.com>
2018-08-30 01:53:21 +0800

    石控制头旋转的脚步

---
+++ Assets/Scripts/Logic/Reaction/RotateAroundMovement.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace MiniGame
+{
+    /// <summary>
+    /// 绕着某个点来回旋转，石头的旋转效果
+    /// </summary>
+    public class RotateAroundMovement : MonoBehaviour
+    {
+        //围绕旋转的中心点
+        public Transform centerPoint;
+        //绕着中心点旋转的速度
+        public float angularSpeed = 10;
+    
+        private Vector3 centerPointPosition;
+        //GameObject移动时候的位置    
+        private Transform cachedTransform;
+        //旋转的方向
+        private float rotatedDir;
+
+
+        private void Awake()
+        {
+            centerPointPosition = centerPoint.transform.position;
+            cachedTransform = gameObject.transform;
+            gameObject.transform.up = (cachedTransform.position - centerPointPosition).normalized;
+
+            rotatedDir = -1;
+        }
+
+        private void Update()
+        {
+            rotateObj();
+        }
+
+        private void rotateObj()
+        {
+            //中心到物体的向量
+            Vector3 posDir = cachedTransform.position - centerPointPosition;
+            //向量跟物体y轴的夹角，判断是否大于0，对应的是Cos函数的值,这样子让物体在90度之间来回绕着中心点旋转
+            if (Vector3.Dot(posDir, cachedTransform.up) > 0)
+            {
+                //顺时针旋转
+                rotatedDir = -1;
+            }
+            else
+            {
+                //逆时针旋转
+                rotatedDir = 1;
+            }
+
+            cachedTransform.RotateAround(centerPointPosition, rotatedDir * Vector3.forward, Time.deltaTime * angularSpeed);
+        }
+    }
+}
+
---
+++ Assets/Scripts/Logic/Reaction/SelfRotateMovement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelfRotateMovement : MonoBehaviour {
+
+    public float rotateSpeed = 30;
+    private Transform cachedTransform;
+
+    private void Awake()
+    {
+        cachedTransform = gameObject.transform;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        cachedTransform.Rotate(Vector3.forward * Time.deltaTime * rotateSpeed);
+    }
+}
