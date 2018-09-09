b560282410a6d4941d20375f92fa1e5df3c8e16f
rekayou <1904362965@qq.com>
2018-09-10 02:19:17 +0800

    大关卡开头动画

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using MiniGameComm;
+
+namespace MiniGame
+{
+    public class MainMenu : MonoSingleton<MainMenu>
+    {
+        public GameObject StartMenu;
+
+        private void Awake()
+        {
+            //StartMenu.SetActive(false);
+        }
+
+        private void Start()
+        {
+            StartCoroutine(StartGame(2.5f));
+        }
+
+        IEnumerator StartGame(float time)
+        {
+            yield return new WaitForSeconds(time - 1.0f);
+            StartMenu.GetComponent<Animator>().SetTrigger("play");
+
+            yield return new WaitForSeconds(1.0f);
+            StartMenu.SetActive(false);                   
+        }
+    }
+
+}
---
+++ Assets/Scripts/UI/MenuStartScreen.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+namespace MiniGame
+{
+    public class MenuStartScreen : MonoBehaviour
+    {
+
+        public Text levelName;
+        // Use this for initialization
+        void Start()
+        {
+            levelName.text = "Level " + MissionManager.Instance.mCurLevel;                      
+         }
+      
+    }
+
+}
