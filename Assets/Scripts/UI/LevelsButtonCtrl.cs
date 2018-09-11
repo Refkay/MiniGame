f2a9aa652e9d4cbf3eadd40c8cd8b0a6ddc0419b
zhiruili <zr.public@outlook.com>
2018-09-11 17:29:53 +0800

    MainMenu

---
+++ Assets/Scripts/UI/LevelsButtonCtrl.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelsButtonCtrl : MonoBehaviour {
+
+	void Start() {
+        var btn = GetComponent<Button>();
+        if (!CheckHasPlayed())
+        {
+            btn.interactable = false;
+        } 
+        btn.onClick.AddListener(OnClick);
+	}
+	
+    // TODO: 检查是否玩过
+    bool CheckHasPlayed()
+    {
+        Debug.LogError("LevelsButtonCtrl::CheckHasPlayed: no implementation!!!");
+        return false;
+    }
+
+    private void OnClick()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("SelectLevel");
+    }
+}
--- Assets/Scripts/UI/StartButtonCtrl.cs
+++ Assets/Scripts/UI/StartButtonCtrl.cs
@@ -1,21 +1,40 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class StartButtonCtrl : MonoBehaviour
 {
 
-    void Awake()
+    void Start()
     {
-        if (!PlayerProgress.Instance.HasPlayed)
+        var btn = GetComponent<Button>();
+        if (!CheckHasPlayed())
         {
-            var lb = transform.FindChild("Label").gameObject;
-            //lb.GetComponent<UILabel>().text = "NEW GAME";
+            var lb = transform.FindChild("StartButtonLabel").gameObject;
+            lb.GetComponent<Text>().text = "Start";
+            btn.onClick.AddListener(LoadStoryScene);
+        } else
+        {
+            btn.onClick.AddListener(LoadRecentScene);
         }
-        UIEventListener.Get(gameObject).onClick = OnStartButtonClick;
     }
 
-    void OnStartButtonClick(GameObject obj)
+    // TODO: 读取最近的关卡
+    void LoadRecentScene()
+    {
+        Debug.LogError("StartButtonCtrl::LoadRecentScene: no implementation!!!");
+    }
+
+    // 读取故事关
+    void LoadStoryScene()
     {
         SceneManager.LoadScene("Level0Story");
+    }
+
+    // TODO: 检查是否玩过
+    bool CheckHasPlayed()
+    {
+        Debug.LogError("StartButtonCtrl::CheckHasPlayed: no implementation!!!");
+        return false;
     }
 }
