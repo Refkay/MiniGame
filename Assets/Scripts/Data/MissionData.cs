49352ad4c2e093cd963d90ebc487baf2b56410ab
zhiruili <zr.public@outlook.com>
2018-09-12 10:10:58 +0800

    Level 3

--- Assets/Scripts/Data/MissionData.cs
+++ Assets/Scripts/Data/MissionData.cs
@@ -25,6 +25,7 @@ namespace MiniGame
             //加载关卡信息
             mMissionDataDic.Add(1, 3);
             mMissionDataDic.Add(2, 5);
+            mMissionDataDic.Add(3, 5);
 
             //加载Player每一小关的起始位置信息
             mPlayerPositionDic.Add("1-1", new Vector3(0f, -9f, 0f));
@@ -37,6 +38,12 @@ namespace MiniGame
             mPlayerPositionDic.Add("2-4", new Vector3(21.7f, -29.6f, 0));
             mPlayerPositionDic.Add("2-5", new Vector3(62.5f, -36.6f, 0));
 
+            mPlayerPositionDic.Add("3-1", new Vector3(0f, -9f, 0f));
+            mPlayerPositionDic.Add("3-2", new Vector3(21.1f, 19, 0));
+            mPlayerPositionDic.Add("3-3", new Vector3(49.4f, 9.4f, 0));
+            mPlayerPositionDic.Add("3-4", new Vector3(21.7f, -29.6f, 0));
+            mPlayerPositionDic.Add("3-5", new Vector3(62.5f, -36.6f, 0));
+
             //加载Camera每一小关的位置
             mCameraPositonDic.Add("1-1", new Vector3(0f, 0f, -10));
             mCameraPositonDic.Add("1-2", new Vector3(31.4f, -10.5f, -10));
@@ -47,6 +54,12 @@ namespace MiniGame
             mCameraPositonDic.Add("2-3", new Vector3(49.4f, 18.4f, -10));
             mCameraPositonDic.Add("2-4", new Vector3(21.7f, -20.6f, -10));
             mCameraPositonDic.Add("2-5", new Vector3(62.5f, -27.6f, -10));
+
+            mCameraPositonDic.Add("3-1", new Vector3(0f, 0f, -10));
+            mCameraPositonDic.Add("3-2", new Vector3(21.1f, 28, -10));
+            mCameraPositonDic.Add("3-3", new Vector3(49.4f, 18.4f, -10));
+            mCameraPositonDic.Add("3-4", new Vector3(21.7f, -20.6f, -10));
+            mCameraPositonDic.Add("3-5", new Vector3(62.5f, -27.6f, -10));
         }
        
         /// <summary>
