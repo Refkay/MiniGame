f01bb2bf5db8e3415ce1a58c2e6dbcdfc5d16acd
zhiruili <zr.public@outlook.com>
2018-09-15 14:58:31 +0800

    fade in out

---
+++ Assets/Scripts/UI/FadeIn.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+
+public class FadeIn: MonoBehaviour {
+
+    public float fromAlpha = 0.0f;
+    public float toAlpha = 1.0f;
+    public float durationSecond = 1.0f;
+    public float delaySecond = 0.0f;
+
+	void Start () {
+        var image = GetComponent<Image>();
+        var color = image.material.color;
+        image.material.color = new Color(color.r, color.g, color.b, toAlpha);
+        image.DOFade(fromAlpha, durationSecond).SetDelay(delaySecond).From();
+	}
+}
--- Assets/Scripts/UI/TimerToMainMenu.cs
+++ Assets/Scripts/UI/TimerToMainMenu.cs
@@ -3,13 +3,15 @@ using System.Collections;
 
 public class TimerToMainMenu : MonoBehaviour {
 
+    public float delay = 3.0f;
+
 	void Start () {
         StartCoroutine(WaitAndStartMainMenu());
     }
 
     private IEnumerator WaitAndStartMainMenu()
     {
-        yield return new WaitForSeconds(3);
+        yield return new WaitForSeconds(delay);
         UnityEngine.SceneManagement.SceneManager.LoadSceneAsync("MainMenu");
     }
 }
